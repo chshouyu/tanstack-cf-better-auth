e62396e62f5bc3f97bca3719a700773c93ed7891
chshouyu <chshouyu89@gmail.com>
2026-03-27 01:58:12 +0800

    init

---
+++ src/router.tsx
@@ -0,0 +1,22 @@
+import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+
+import { routeTree } from '@/routeTree.gen'
+
+export function getRouter() {
+  const router = createTanStackRouter({
+    routeTree,
+
+    scrollRestoration: true,
+    defaultPreload: 'intent',
+    defaultPreloadStaleTime: 0,
+    defaultNotFoundComponent: () => <div>Not Found</div>,
+  })
+
+  return router
+}
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof getRouter>
+  }
+}
